e25d43dd94d06e131787711522396293a35f80fd
samAricha <samaricha001.com>
2023-05-17 14:19:00 +0300

    HomeView model created

---
+++ app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/home/HomeViewModel.kt
@@ -0,0 +1,14 @@
+package teka.android.smoothrufaaandroidclient.presentation.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import teka.android.smoothrufaaandroidclient.domain.repository.FacilityRepository
+import javax.inject.Inject
+
+
+@HiltViewModel
+class HomeViewModel  @Inject constructor(): ViewModel() {
+
+
+
+}
