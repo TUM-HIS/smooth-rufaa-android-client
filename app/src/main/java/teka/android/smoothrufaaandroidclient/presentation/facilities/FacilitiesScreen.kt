dff36603a0f48778d4d67fd15c86db5b8506bba5
samAricha <samaricha001.com>
2023-05-18 13:42:31 +0300

    added lazy column for facilities

--- app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilities/FacilitiesScreen.kt
+++ app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilities/FacilitiesScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,13 +15,26 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun FacilitiesScreen(){
 
-    Column(
+    val facilities = repeat(5) {
+        FacilityCard()
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+
+
+    Scaffold(
         modifier = Modifier.padding(16.dp)
     ) {
-        repeat(5) {
-            FacilityCard()
-            Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn{
+
+            items(5){
+
+                    FacilityCard()
+                    Spacer(modifier = Modifier.height(16.dp))
+
+            }
+
         }
+
     }
 
 }
