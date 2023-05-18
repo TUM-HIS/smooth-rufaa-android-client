548c250c80f85a1628d279a8537c2ca99aea83a5
samAricha <samaricha001.com>
2023-05-18 14:10:29 +0300

    card error in facility list page solved

--- app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilities/FacilitiesScreen.kt
+++ app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilities/FacilitiesScreen.kt
@@ -1,11 +1,9 @@
 package teka.android.smoothrufaaandroidclient.presentation.facilities
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,12 +12,6 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun FacilitiesScreen(){
-
-    val facilities = repeat(5) {
-        FacilityCard()
-        Spacer(modifier = Modifier.height(16.dp))
-    }
-
 
     Scaffold(
         modifier = Modifier.padding(16.dp)
--- app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilities/FacilityCard.kt
+++ app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilities/FacilityCard.kt
@@ -1,24 +1,17 @@
 package teka.android.smoothrufaaandroidclient.presentation.facilities
 
-import android.media.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.material3.*
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 
 import androidx.compose.ui.unit.dp
-import teka.android.smoothrufaaandroidclient.R
 
 
 @Composable
