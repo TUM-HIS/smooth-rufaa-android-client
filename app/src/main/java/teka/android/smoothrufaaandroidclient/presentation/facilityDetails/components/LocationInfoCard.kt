e7dfa611277e48a081de405eb29e9a2841426629
samAricha <samaricha001.com>
2023-05-20 13:34:17 +0300

    started LocationInfoCard.kt

--- app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilityDetails/components/LocationInfoCard.kt
+++ app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilityDetails/components/LocationInfoCard.kt
@@ -1,2 +1,25 @@
 package teka.android.smoothrufaaandroidclient.presentation.facilityDetails.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LocationInfoCard() {
+
+
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+
+    }
+}
