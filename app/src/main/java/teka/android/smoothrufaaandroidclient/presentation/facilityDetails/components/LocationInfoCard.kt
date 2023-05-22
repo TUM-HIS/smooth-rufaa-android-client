6c721268a2135ad9b121867be7f14ee4be6c1492
samAricha <samaricha001.com>
2023-05-22 12:15:23 +0300

    LocationInfoCard statically done

--- app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilityDetails/components/LocationInfoCard.kt
+++ app/src/main/java/teka/android/smoothrufaaandroidclient/presentation/facilityDetails/components/LocationInfoCard.kt
@@ -1,12 +1,23 @@
 package teka.android.smoothrufaaandroidclient.presentation.facilityDetails.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import teka.android.smoothrufaaandroidclient.R
 
 @Composable
 fun LocationInfoCard() {
@@ -17,9 +28,57 @@ fun LocationInfoCard() {
         elevation = CardDefaults.cardElevation(
             defaultElevation = 4.dp
         ),
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Gray)
+            .padding(6.dp)
     ) {
+
+        Row() {
+
+
+            Column(
+                modifier = Modifier.weight(2f)
+            ) {
+
+                Text(text = "Location")
+                
+                Text(text = "Muranga COUNTY")
+                
+                Text(text = "KANGEMA SUB-COUNTY")
+                
+                Text(text = "Kanyenya-ini WARD")
+                
+                Text(text = "Karurumo shopping centre town")
+
+            }
+
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(textAlign = TextAlign.Center,
+                    text = "Show location on a map")
+                Image(
+                    painter = painterResource(id = R.drawable.map),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(72.dp)
+                )
+
+            }
+
+        }
+
 
 
     }
+}
+
+
+@Preview
+@Composable
+fun SimpleComposablePreview() {
+    LocationInfoCard()
 }
